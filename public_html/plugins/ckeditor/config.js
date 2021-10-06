2e7fcf645ee22a4d7a160d4f82db174d0b6e37b1
Payam Yasaie <payam.yasaie@outlook.com>
2021-10-06 17:51:34 +0330

    [ADD] JS plugins

---
+++ public_html/plugins/ckeditor/config.js
@@ -0,0 +1,23 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	//config.language = 'fa';
+
+	config.contentsLangDirection = 'rtl';
+
+	// Simplify the dialog windows.
+	//config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	config.filebrowserUploadMethod = 'form';
+	config.filebrowserBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=files';
+	config.filebrowserImageBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=images';
+	config.filebrowserFlashBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=flash';
+	config.filebrowserUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=files';
+	config.filebrowserImageUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=images';
+	config.filebrowserFlashUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=flash';
+
+};
